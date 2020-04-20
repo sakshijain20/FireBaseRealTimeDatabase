e60f17119b9bf9b3ecec3a2b48c5c143a71565e8
Sakshi Madrecha <38380683+sakshijain20@users.noreply.github.com>
2020-04-20 15:54:31 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/firebaserealtiemdatabase/MainActivity.java
+++ app/src/main/java/com/example/firebaserealtiemdatabase/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.firebaserealtiemdatabase;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,8 +9,10 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference dbReference;
     public String firstname,lastname,Address;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +37,20 @@ public class MainActivity extends AppCompatActivity {
         lastName= (EditText) findViewById(R.id.lastName);
         address= (EditText) findViewById(R.id.address);
         add=(Button) findViewById (R.id.add);
-
+        progressBar=(ProgressBar) findViewById (R.id.progressbar);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    insertData();
+                progressBar.setVisibility(View.VISIBLE);
+                    if(phoneNumber.getText().toString().length()!=10){
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(MainActivity.this, "Invalid phone number", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        progressBar.setVisibility(View.GONE);
+                        insertData();
+                    }
+
             }
         });
     }
@@ -46,16 +60,49 @@ public class MainActivity extends AppCompatActivity {
         lastname= lastName.getText().toString().trim();
         Address = address.getText().toString().trim();
 
+
         if(!TextUtils.isEmpty(firstname) && !TextUtils.isEmpty(lastname) && !TextUtils.isEmpty(Address)){
             database=FirebaseDatabase.getInstance();
-            dbReference=database.getReference("Users").push();
+            dbReference=database.getReference("Users");
+
+            progressBar.setVisibility(View.VISIBLE);
+
             User user=new User(firstname,lastname,Address);
-            dbReference.child(phoneNumber.getText().toString().trim()).setValue(user);
-            Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+            dbReference.child(phoneNumber.getText().toString().trim()).setValue(user, new DatabaseReference.CompletionListener() {
+                @Override
+                public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                    progressBar.setVisibility(View.GONE);
+                    if(databaseError!=null){
+                        Toast.makeText(MainActivity.this, "Some error occurred", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Data inserted successfully!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
 
         }
         else{
-            Toast.makeText(this, "All field are required!!", Toast.LENGTH_SHORT).show();
+
+            if(TextUtils.isEmpty(firstname)){
+                firstName.setError("First name required");
+                firstName.requestFocus();
+            }
+            if(TextUtils.isEmpty(lastname)){
+                lastName.setError("Last name required");
+                lastName.requestFocus();
+            }
+            if(TextUtils.isEmpty(Address)){
+                address.setError("Address required");
+                address.requestFocus();
+            }
+            else{
+                Toast.makeText(this, "All field are required!!", Toast.LENGTH_SHORT).show();
+                firstName.requestFocus();
+                lastName.requestFocus();
+                address.requestFocus();
+            }
+
         }
 
 
