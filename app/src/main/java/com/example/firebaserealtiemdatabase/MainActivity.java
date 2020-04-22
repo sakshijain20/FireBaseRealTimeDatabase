9f115d80c7a1b9ecdc111b452ea0f99adccdb5b3
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2020-04-23 01:32:25 +0530

    trying to retrieve data.

--- app/src/main/java/com/example/firebaserealtiemdatabase/MainActivity.java
+++ app/src/main/java/com/example/firebaserealtiemdatabase/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.firebaserealtiemdatabase;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
@@ -20,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
     Button add;
     FirebaseDatabase database;
     DatabaseReference dbReference;
-    public String firstname,lastname,Address;
+    public String firstname,lastname,Address,phone;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +40,20 @@ public class MainActivity extends AppCompatActivity {
         lastName= (EditText) findViewById(R.id.lastName);
         address= (EditText) findViewById(R.id.address);
         add=(Button) findViewById (R.id.add);
-
+        progressBar=(ProgressBar) findViewById (R.id.progressbar);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    insertData();
+                progressBar.setVisibility(View.VISIBLE);
+                    if(phoneNumber.getText().toString().length()!=10){
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(MainActivity.this, "Invalid phone number", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        progressBar.setVisibility(View.GONE);
+                        insertData();
+                    }
+
             }
         });
     }
@@ -45,19 +62,72 @@ public class MainActivity extends AppCompatActivity {
         firstname= firstName.getText().toString().trim();
         lastname= lastName.getText().toString().trim();
         Address = address.getText().toString().trim();
+    phone=phoneNumber.getText().toString().trim();
 
         if(!TextUtils.isEmpty(firstname) && !TextUtils.isEmpty(lastname) && !TextUtils.isEmpty(Address)){
             database=FirebaseDatabase.getInstance();
-            dbReference=database.getReference("Users").push();
+            dbReference=database.getReference("Users");
+
+            progressBar.setVisibility(View.VISIBLE);
+
             User user=new User(firstname,lastname,Address);
-            dbReference.child(phoneNumber.getText().toString().trim()).setValue(user);
-            Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+            dbReference.child(phoneNumber.getText().toString().trim()).setValue(user, new DatabaseReference.CompletionListener() {
+                @Override
+                public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                    progressBar.setVisibility(View.GONE);
+                    if(databaseError!=null){
+                        Toast.makeText(MainActivity.this, "Some error occurred", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Data inserted successfully!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
 
         }
         else{
-            Toast.makeText(this, "All filed are required!!", Toast.LENGTH_SHORT).show();
+
+            if(TextUtils.isEmpty(firstname)){
+                firstName.setError("First name required");
+                firstName.requestFocus();
+            }
+            if(TextUtils.isEmpty(lastname)){
+                lastName.setError("Last name required");
+                lastName.requestFocus();
+            }
+            if(TextUtils.isEmpty(Address)){
+                address.setError("Address required");
+                address.requestFocus();
+            }
+            else{
+                Toast.makeText(this, "All field are required!!", Toast.LENGTH_SHORT).show();
+                firstName.requestFocus();
+                lastName.requestFocus();
+                address.requestFocus();
+            }
+
         }
 
 
+    }
+
+    public void retrieve_data(View view) {
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = (DatabaseReference) database.getReference("Users").orderByKey();
+
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String user = dataSnapshot.getKey();
+                System.out.println(String.valueOf(user));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
     }
 }
